3c426c8b48c17c9e846fb134d4e8f1939512d854
jmendezaktiva-arch <jmendez.aktiva@gmail.com>
2026-01-21 23:09:45 -0600

    Delete js directory

--- js/config.js
+++
@@ -1,9 +0,0 @@
-// js/config.js
-import { getIcon } from './utils.js';
-
-export const sectionsData = [
-    // --- Parte 1: Finanzas y Diagnóstico ---
-    { id: 'ej1', title: '1. Diagnóstico de Consolidación de Finanzas', icon: getIcon('chart') },
-    { id: 'ej2', title: '2. Plan de Acción para la Consolidación', icon: getIcon('clipboard') },
-    
-];
