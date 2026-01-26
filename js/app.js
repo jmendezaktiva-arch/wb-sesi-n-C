f7ed2ac059fc6021589bdbd0aa549c75745893e3
jmendezaktiva-arch <jmendez.aktiva@gmail.com>
2026-01-26 09:51:01 -0600

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -616,7 +616,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const sectionsData = [
         { id: 'ej1', title: '1. Diagnóstico de Consolidación', icon: 'M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2' },
         { id: 'ej2', title: '2. Plan de Acción', icon: 'M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z' },
-        { id: 'ej3', title: '3. Gestión de Inversiones', icon: 'M13 7h8m0 0v8m0-8l-8 8-4-4-6 6' },
+        { id: 'ej3', title: '3. Autoevaluación de gestión de inversiones', icon: 'M13 7h8m0 0v8m0-8l-8 8-4-4-6 6' },
         { id: 'ej4', title: '4. Flujo de Caja Libre', icon: 'M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z' },
         { id: 'ej5', title: '5. Prioridades de Negocio', icon: 'M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z' },
         { id: 'ej6', title: '6. Evaluación del Rendimiento', icon: 'M13 7h8m0 0v8m0-8l-8 8-4-4-6 6' },
